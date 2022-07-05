0fd31ae6eaf95bac2e41a08fe0e1db50f8ad613a
Axurynn <marion.bricout@outlook.com>
2022-07-05 15:02:02 +0200

    init structure

---
+++ src/app/pages/car-reservation-page/car-reservation.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarReservationPage } from './car-reservation.page';
+
+@NgModule({
+  declarations: [CarReservationPage],
+  imports: [CommonModule],
+  exports: [CarReservationPage],
+})
+export class CarReservationModule {}
---
+++ src/app/pages/car-reservation-page/car-reservation.page.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarReservationPage } from './car-reservation.page';
+
+describe('CarReservationPage', () => {
+  let component: CarReservationPage;
+  let fixture: ComponentFixture<CarReservationPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CarReservationPage],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CarReservationPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
