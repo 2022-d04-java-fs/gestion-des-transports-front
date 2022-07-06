e8e3475780bb13164d35f883c1f7667588a25e64
Etienne Renoton <etienne.renoton@gmail.com>
2022-07-06 15:11:48 +0200

    OfferList Front

--- src/app/offer-list/offer-list.component.ts
+++ src/app/offer-list/offer-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+const FILTER_PAG_REGEX = /[^0-9]/g;
 @Component({
   selector: 'app-offer-list',
   templateUrl: './offer-list.component.html',
@@ -7,9 +8,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferListComponent implements OnInit {
 
-  constructor() { }
+  public isCollapsed = false;
+  public isModal = true;
 
+  public offerList = [
+    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 12:30",
+      marque: "",
+      immatriculation: "",
+      modèle:"",
+      nbVoyageurs:3,
+    },
+    {
+      depart: "Gare de Nantes",
+      destination: "Nantes Centre",
+      date: "18/06/2017 14:30",
+      marque: "",
+      immatriculation: "",
+      modèle:"",
+      nbVoyageurs:8,
+    }
+  ]
+
+  public historyList = [
+    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 12:30",
+      marque: "",
+      immatriculation: "",
+      modèle:"",
+      nbVoyageurs:3,
+    },
+    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 14:30",
+      marque: "",
+      immatriculation: "",
+      modèle:"",
+      nbVoyageurs:1,
+    },
+    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 12:30",
+      vehicule: "",
+      chauffeur: "",
+      nbVoyageurs:4,
+    },
+    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 14:30",
+      vehicule: "",
+      chauffeur: "",
+      nbVoyageurs:3,
+    },    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 12:30",
+      vehicule: "",
+      chauffeur: "",
+      nbVoyageurs:5,
+    },
+    {
+      depart: "Gare de Nantes",
+      destination: "Gare de Saint-Nazaire",
+      date: "22/06/2017 14:30",
+      vehicule: "",
+      chauffeur: "",
+      nbVoyageurs:2,
+    },
+  ]
+
+  closeResult = '';
+  page = 1;
+  pageSize = 3;
+  modalTable: string[] = ['', '', '', '', ''];
+
+
+  constructor(private modalService: NgbModal) { }
   ngOnInit(): void {
   }
+  /**
+   * Parameters: content: any
+   * Cette fonction permettra d'ouvrir la fenêtre modale avec Angular
+   */
+  open(content: any): void {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  getListeValues(objet: any) {
+    objet
+  }
+    selectPage(page: string) {
+      this.page = parseInt(page, 10) || 1;
+    }
+
+    formatInput(input: HTMLInputElement) {
+      input.value = input.value.replace(FILTER_PAG_REGEX, '');
 
 }
+  }
