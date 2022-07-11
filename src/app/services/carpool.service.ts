c804f1a97e85853984bb0f6cd861e022fea7de8c
Etienne Renoton <etienne.renoton@gmail.com>
2022-07-11 15:56:00 +0200

    carpoolService

--- src/app/services/carpool.service.ts
+++ src/app/services/carpool.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarpoolService {
 
-  constructor() { }
- // listCarpoolByUser(userID:number){
- //   return this.http.get("http://localhost:8080/api/carpools/reservations/:userID" + userID )
- // }
+  constructor(private http:HttpClient) { }
+  listCarpoolByUser(userID:number){
+    return this.http.get("http://localhost:8080/api/carpools/reservations/:userID" + userID ) //url de test, à remplacer par https://gestion-des-transports.herokuapp.com/carpools/reservations/:userID
+
+  }
 }
